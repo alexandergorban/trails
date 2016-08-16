c2ddff88804b4af4b92e5193c87a1b191d7a1623
alexandergorban <gorban.alexander.y@gmail.com>
2016-08-16 21:07:30 +0300

    init

---
+++ wwwroot/app/home.controller.js
@@ -0,0 +1,16 @@
+(function() {
+'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeController', ControllerController);
+
+    function ControllerController() {
+        var vm = this;
+        
+
+        activate();
+
+        function activate() { }
+    }
+})();
